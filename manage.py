a1fee58b979459a102a9f8d13588538237d4e79b
nycarchivalsociety <142239463+nycarchivalsociety@users.noreply.github.com>
2024-04-02 21:23:25 -0800

    Add files via upload

--- manage.py
+++ manage.py
@@ -1,20 +1,8 @@
 from app import create_app
-from app.db.db import execute_sql
-from app.db.models import create_tables_sql
 
-# Set up your connection parameters
-connection_parameters = {
-    "host": "dpg-co2a6e8l6cac739j2mh0-a.ohio-postgres.render.com",
-    "port": "5432",
-    "database": "nyas_db",
-    "user": "nyas_db_user",
-    "password": "QQ9HfLAqRJq6mZRTx6gZF5uZ4jgvXba3",
-    "sslmode": "require"  # Render's databases require SSL.
-}
+app = create_app()
+
+app.secret_key = 'fdkjfhkjfhdkj'
 
 if __name__ == '__main__':
-    # Call the factory function to get the Flask app instance
-    app = create_app()
-    execute_sql(create_tables_sql, connection_parameters)
-    print("Database and tables created successfully.")
-    app.run()
+    app.run(debug=True)
