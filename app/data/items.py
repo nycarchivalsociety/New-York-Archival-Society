ae0d9a26cf0f136bac95915cc40fc134f2992b51
nycarchivalsociety <142239463+nycarchivalsociety@users.noreply.github.com>
2024-04-04 08:16:15 -0800

    Update items.py

--- app/data/items.py
+++ app/data/items.py
@@ -1,74 +1,140 @@
-items = [
-    {
-        'id': 1,
-        'name': 'Brooklyn Bridge Drawing no. 2031, Balance Derrick ca. 1870',
-        'fee': 200,
-        'desc': "Among the Archives' more than 10,000 drawing plans for the Brooklyn Bridge, this drawing details the construction of a derrick used for lifting materials during the construction of the towers. The drawing reflects the remarkable detail and artistry that went into each drawing plan for the bridge. Conservation treatment would help to reduce staining from adhesive damage and remove some of the discoloration and degradation products from the paper, making the paper more stable, safer to handle, and bringing the drawing closer to its original appearance.",
-        'photo': False,
-        'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1710449964/New%20York%20Archival%20Society/Items/brooklyn_bridge_drawing_no._2031_zfhkns.jpg'
+import uuid 
 
-    },
+items = [
+    # {
+    #     'id': 1,
+    #     'name': 'Brooklyn Bridge Drawing no. 2031, Balance Derrick ca. 1870',
+    #     'fee': 200,
+    #     'desc': "Among the Archives' more than 10,000 drawing plans for the Brooklyn Bridge, this drawing details the construction of a derrick used for lifting materials during the construction of the towers. The drawing reflects the remarkable detail and artistry that went into each drawing plan for the bridge. Conservation treatment would help to reduce staining from adhesive damage and remove some of the discoloration and degradation products from the paper, making the paper more stable, safer to handle, and bringing the drawing closer to its original appearance.",
+    #     'photo': False,
+    #     'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1710449964/New%20York%20Archival%20Society/Items/brooklyn_bridge_drawing_no._2031_zfhkns.jpg'
+
+    # },
     
     {
-        'id': 2,
+        'id': '5b67e549-5c01-418a-b1cb-9097e7932c2e',
         'name': 'Brooklyn Bridge Drawing no. 4275, Caisson Construction',
         'fee': 200,
         'desc': "Among the Archives' more than 10,000 drawing plans for the Brooklyn Bridge, this drawing depicts equipment custom built as part of the underwater caisson to remove rock from beneath the riverbed. The drawing reflects the remarkable detail and artistry that went into each drawing plan for the bridge. Conservation treatment would help to remove some of the discoloration and degradation products from the paper, making the paper more stable, safer to handle, and bringing the drawings closer to their original appearance.",
         'photo': True,
+        'adopted': False,
         'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1712109063/New%20York%20Archival%20Society/Items/brooklyn_bridge_drawing_no._4275_syuybo.jpg'
 
     },
 
 
   {
-        'id': 3,
+        'id': '831935d8-4c69-4122-963e-0162d6290360',
         'name': 'Brooklyn Bridge Drawing no. 4275, Caisson Construction',
         'fee': 200,
         'desc': "Among the Archives' more than 10,000 drawing plans for the Brooklyn Bridge, this drawing depicts equipment custom built as part of the underwater caisson to remove rock from beneath the riverbed. The drawing reflects the remarkable detail and artistry that went into each drawing plan for the bridge. Conservation treatment would help to remove some of the discoloration and degradation products from the paper, making the paper more stable, safer to handle, and bringing the drawings closer to their original appearance.",
         'photo': True,
+        'adopted': False,
         'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1712109063/New%20York%20Archival%20Society/Items/brooklyn_bridge_drawing_no._4275_syuybo.jpg'
 
     },
  {
-        'id': 4,
+        'id': '712beb8b-906b-4c88-a175-818994e6f5c5',
         'name': 'Land Patent of New Ultrech 1686',
         'fee': 200,
         'desc': "When Thomas Dongan became governor of New York in 1683, he made many notable changes in the region, including the 'Charter of Liberties,' which granted citizens broader rights and privileges. Dongan also issued land grants that established towns and their boundaries throughout New York, known as the Dongan Patents. The Archives holds several of these original patents in its collections. Hand-written in iron gall ink on parchment (processed animal skin), the patents, issued under King James II, established town trustees as the governing bodies for managing common land. These patents are still upheld by the law today. The 1686 patent for New Utrecht exhibits many signs of deterioration common to aged parchment documents, including numerous creases and folds as a result of its sensitivity to fluctuations in humidity, making the document difficult to read. Adopting this item would support conservation efforts to smooth folded and cockled parts of the document, remove previous repairs that have aged poorly, and provide proper housing for its long-term preservation.",
         'photo': True,
+        'adopted': False,
         'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1710461246/New%20York%20Archival%20Society/Items/land_patent_new_utrecht_ss4ifc.jpg'
 
     },
  
  {
-        'id': 5,
+        'id': 'fdc1319b-0bd2-421d-8cb7-181501285cf5',
         'name': 'Newtown Town records volume 288',
         'fee': 450,
         'desc': "Part of the Archive's 'Old Town' collection, the contents of this volume date from 1714 - 1753 and detail the daily town business of Newtown Queens during colonial era New York. Written in iron gall ink, the paper and binding have suffered considerable damage. Conservation treatment is necessary to stabilize the ink to slow its deterioration and a create a new binding to make the volume safe for researchers to handle.",
         'photo': True,
+        'adopted': False,
         'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1710449900/New%20York%20Archival%20Society/Items/newtown_town_records_vol._288_syput3.jpg'
 
     },
  
   
  {
-        'id': 6,
+        'id': '2390e83f-70de-4aca-b4b0-4c50b9cd8ff0',
         'name': 'Code of Health Ordinance 1866',
         'fee': 175,
         'desc': "Poor sanitary conditions and a massive outbreak of cholera prompted the formation of the Metropolitan Board of Health in New York in 1866. The Municipal Library's original copy of the board's first Code of Health Ordinances possesses some unique features, including handwritten notes on ordinances concerning the protection of animals and an ASPCA book plate depicting an angel preventing the harm of a carriage horse. The volume, however, has been damaged, with detached covers and a missing spine. Adopting this book will support its preservation and custom housing.",
         'photo': True,
+        'adopted': False,
         'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1710461446/New%20York%20Archival%20Society/Items/code_of_health_ordinances_1866_fjoa8t.jpg'
 
     },
  
    
  {
-        'id': 6,
+        'id': 'dde21d40-2dfe-41be-9b3b-e54252ea7ae1',
         'name': 'Brooklyn Bridge Drawing 4269',
         'fee': 250,
         'desc': "Among the Archives' more than 10,000 drawing plans for the Brooklyn Bridge, this drawing by famed draftsman Wilhelm Hildenbrand details specifics of a travelling crane for moving materials in the construction of the Brooklyn Bridge. Drawn in ink and watercolor, the drawing reflects the remarkable detail and artistry that went into each drawing plan for the bridge. Conservation treatment would help to remove some of the discoloration and degradation products from the paper, making the paper more stable, safer to handle, and bringing the drawings closer to their original appearance.",
         'photo': True,
+        'adopted': False,
         'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1710461246/New%20York%20Archival%20Society/Items/land_patent_new_utrecht_ss4ifc.jpg'
 
-    },
+},
+ 
+ #adopted items
+  {
+        'id': '9472695f-d42a-4bd4-be59-d5e2ebc0aa1f',
+        'name': 'Annual Report of the Board of Commissioners of the Central Park (1856 - 1870), 12 vols.',
+        'fee': 70.10,
+        'desc': "The Municipal Library's collection of Annual Reports of Central Park document the early development and use of the park in the mid to late 1800s in vivid detail. The volumes also each contain fine lithographic foldout prints throughout. The earliest rare volumes are in need of custom enclosures that will protect the often delicate contents. Adopting one or more of these volumes will support their long term preservation by helping us to build cloth-covered drop-spine boxes to store them in.",
+        'photo': True,
+        'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1710460207/New%20York%20Archival%20Society/Items/Annual_Report_of_the_Board_of_Commissioners_of_the_Central_Park_naoidg.jpg',
+        'adopted': True,
+        'donors': [
+             'Kevin Foley',
+         ]
+
+},
+  
+  
+    {
+        'id': '22283ffe-067a-41b7-98f2-2ae037f2e029',
+        'name': 'Slave Register, Town of Flatlands, 1799',
+        'fee': 282.00,
+        'desc': "This volume, which documents the birth of slaves in the Flatlands area of Brooklyn, is a unique record of New York History. Created during the period in which slavery was gradually abolished in the state, the volume contains the hand-written birth certificates of children born to enslaved women. The binding of this volume has been damaged over time, and requires conservation treatment to make it safer for researchers to handle. Repairing the binding and creating a custom enclosure will ensure that the volume is protected both from poor handling and external environmental conditions.",
+        'photo': True,
+        'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1710449912/New%20York%20Archival%20Society/Items/slave_register__town_of_flatlands__1799_dxxc20.jpg',
+        'adopted': True,
+        'donors': [
+             'Betty Kay',
+         ]
+
+},
+    
+    {
+        'id': 'dfedc014-280b-4005-b400-cce4a137145e',
+        'name': 'Central Park Drawing no. 35 Belvedere Castle Front Elevation',
+        'fee': 200.00,
+        'desc': "The Archives’ collection includes roughly 2000 drawings related to the planning and construction of Central Park. These two drawings of the front and west elevations of Belvedere Castle, designed by Frederick Law Olmsted and Calvert Vaux, represent an early rendering of the structure and its detailed finial. Over time the drawings' have become weakened and discolored by acids due to lignin content in the paper. Conservation treatment would reduce the acid and discoloration in the paper, and a new tissue lining on the reverse would add needed strength.",
+        'photo': True,
+        'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1710459670/New%20York%20Archival%20Society/Items/Central_Park_Drawing_no._35_1_lgxl1d.jpg',
+        'adopted': True,
+        'donors': [
+             'Nancy Hughes',
+         ]
+
+},
+    
+     {
+        'id': 'cc706ec5-048b-41cf-aa69-dde76d3fa9ba',
+        'name': 'Central Park Drawing no. 36 Belvedere Castle West Elevation',
+        'fee': 200.00,
+        'desc': "The Archives’ collection includes roughly 2000 drawings related to the planning and construction of Central Park. These two drawings of the front and west elevations of Belvedere Castle, designed by Frederick Law Olmsted and Calvert Vaux, represent an early rendering of the structure and its detailed finial. Over time the drawings' have become weakened and discolored by acids due to lignin content in the paper. Conservation treatment would reduce the acid and discoloration in the paper, and a new tissue lining on the reverse would add needed strength.",
+        'photo': True,
+        'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1710460005/New%20York%20Archival%20Society/Items/central_park_drawing_no._35_2_e69ly5.jpg',
+        'adopted': True,
+        'donors': [
+             'Nancy Hughes',
+         ]
+
+},
  
 ]
