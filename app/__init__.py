249e28e864b0085b9ef938e4ee339eb7b4e7ff54
nycarchivalsociety <142239463+nycarchivalsociety@users.noreply.github.com>
2024-04-03 16:54:10 -0800

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,7 @@ def create_app():
     app = Flask(__name__)
     app.secret_key = b'\xf7\xb1}\xf3T"\x8a\x95\xf3{\x01\x0c\x82\xab\x84\xc4\xac\xfe1\x11\x14\x7f\x8b\xb4'
     
-    # Import blueprints
     from .routes.main import main as main_blueprint
-
-    # Register blueprints
     app.register_blueprint(main_blueprint)
-
 
     return app
--- app/routes/main/__init__.py
+++ app/routes/main/__init__.py
@@ -1,6 +1,6 @@
-# In app/routes/main/__init__.py
+# app/routes/main/__init__.py
 from flask import Blueprint
 
 main = Blueprint('main', __name__)
 
-from . import views  # Import views after initializing Blueprint
+from . import views
