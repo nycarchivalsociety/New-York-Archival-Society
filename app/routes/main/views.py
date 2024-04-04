4c937da014d7b73e3a9576641d5d857b94fe68bf
nycarchivalsociety <142239463+nycarchivalsociety@users.noreply.github.com>
2024-04-04 08:16:46 -0800

    Update views.py

--- app/routes/main/views.py
+++ app/routes/main/views.py
@@ -3,7 +3,7 @@ from . import main
 from flask import render_template
 from dotenv import load_dotenv
 import os
-
+from app.data.items import items
 
 image_urls = [
     "https://images.squarespace-cdn.com/content/v1/57ade572bebafb370ceb883f/1499865908939-KIXLCIC5P6MX75NTD1DY/image-asset.jpeg?format=750w",
@@ -38,82 +38,7 @@ def boardofdirectors():
 @main.route('/projects')
 def projects():
     return render_template('projects.html')
-
-
-items = [
-    {
-        'id': 1,
-        'name': 'Brooklyn Bridge Drawing no. 2031, Balance Derrick ca. 1870',
-        'fee': 200,
-        'desc': "Among the Archives' more than 10,000 drawing plans for the Brooklyn Bridge, this drawing details the construction of a derrick used for lifting materials during the construction of the towers. The drawing reflects the remarkable detail and artistry that went into each drawing plan for the bridge. Conservation treatment would help to reduce staining from adhesive damage and remove some of the discoloration and degradation products from the paper, making the paper more stable, safer to handle, and bringing the drawing closer to its original appearance.",
-        'photo': False,
-        'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1710449964/New%20York%20Archival%20Society/Items/brooklyn_bridge_drawing_no._2031_zfhkns.jpg'
-
-    },
-    
-    {
-        'id': 2,
-        'name': 'Brooklyn Bridge Drawing no. 4275, Caisson Construction',
-        'fee': 200,
-        'desc': "Among the Archives' more than 10,000 drawing plans for the Brooklyn Bridge, this drawing depicts equipment custom built as part of the underwater caisson to remove rock from beneath the riverbed. The drawing reflects the remarkable detail and artistry that went into each drawing plan for the bridge. Conservation treatment would help to remove some of the discoloration and degradation products from the paper, making the paper more stable, safer to handle, and bringing the drawings closer to their original appearance.",
-        'photo': True,
-        'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1712109063/New%20York%20Archival%20Society/Items/brooklyn_bridge_drawing_no._4275_syuybo.jpg'
-
-    },
-
-
-  {
-        'id': 3,
-        'name': 'Brooklyn Bridge Drawing no. 4275, Caisson Construction',
-        'fee': 200,
-        'desc': "Among the Archives' more than 10,000 drawing plans for the Brooklyn Bridge, this drawing depicts equipment custom built as part of the underwater caisson to remove rock from beneath the riverbed. The drawing reflects the remarkable detail and artistry that went into each drawing plan for the bridge. Conservation treatment would help to remove some of the discoloration and degradation products from the paper, making the paper more stable, safer to handle, and bringing the drawings closer to their original appearance.",
-        'photo': True,
-        'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1712109063/New%20York%20Archival%20Society/Items/brooklyn_bridge_drawing_no._4275_syuybo.jpg'
-
-    },
- {
-        'id': 4,
-        'name': 'Land Patent of New Ultrech 1686',
-        'fee': 200,
-        'desc': "When Thomas Dongan became governor of New York in 1683, he made many notable changes in the region, including the 'Charter of Liberties,' which granted citizens broader rights and privileges. Dongan also issued land grants that established towns and their boundaries throughout New York, known as the Dongan Patents. The Archives holds several of these original patents in its collections. Hand-written in iron gall ink on parchment (processed animal skin), the patents, issued under King James II, established town trustees as the governing bodies for managing common land. These patents are still upheld by the law today. The 1686 patent for New Utrecht exhibits many signs of deterioration common to aged parchment documents, including numerous creases and folds as a result of its sensitivity to fluctuations in humidity, making the document difficult to read. Adopting this item would support conservation efforts to smooth folded and cockled parts of the document, remove previous repairs that have aged poorly, and provide proper housing for its long-term preservation.",
-        'photo': True,
-        'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1710461246/New%20York%20Archival%20Society/Items/land_patent_new_utrecht_ss4ifc.jpg'
-
-    },
  
- {
-        'id': 5,
-        'name': 'Newtown Town records volume 288',
-        'fee': 450,
-        'desc': "Part of the Archive's 'Old Town' collection, the contents of this volume date from 1714 - 1753 and detail the daily town business of Newtown Queens during colonial era New York. Written in iron gall ink, the paper and binding have suffered considerable damage. Conservation treatment is necessary to stabilize the ink to slow its deterioration and a create a new binding to make the volume safe for researchers to handle.",
-        'photo': True,
-        'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1710449900/New%20York%20Archival%20Society/Items/newtown_town_records_vol._288_syput3.jpg'
-
-    },
- 
-  
- {
-        'id': 6,
-        'name': 'Code of Health Ordinance 1866',
-        'fee': 175,
-        'desc': "Poor sanitary conditions and a massive outbreak of cholera prompted the formation of the Metropolitan Board of Health in New York in 1866. The Municipal Library's original copy of the board's first Code of Health Ordinances possesses some unique features, including handwritten notes on ordinances concerning the protection of animals and an ASPCA book plate depicting an angel preventing the harm of a carriage horse. The volume, however, has been damaged, with detached covers and a missing spine. Adopting this book will support its preservation and custom housing.",
-        'photo': True,
-        'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1710461446/New%20York%20Archival%20Society/Items/code_of_health_ordinances_1866_fjoa8t.jpg'
-
-    },
- 
-   
- {
-        'id': 6,
-        'name': 'Brooklyn Bridge Drawing 4269',
-        'fee': 250,
-        'desc': "Among the Archives' more than 10,000 drawing plans for the Brooklyn Bridge, this drawing by famed draftsman Wilhelm Hildenbrand details specifics of a travelling crane for moving materials in the construction of the Brooklyn Bridge. Drawn in ink and watercolor, the drawing reflects the remarkable detail and artistry that went into each drawing plan for the bridge. Conservation treatment would help to remove some of the discoloration and degradation products from the paper, making the paper more stable, safer to handle, and bringing the drawings closer to their original appearance.",
-        'photo': True,
-        'imgURL': 'https://res.cloudinary.com/djozxyart/image/upload/v1710461246/New%20York%20Archival%20Society/Items/land_patent_new_utrecht_ss4ifc.jpg'
-
-    },
- 
-]
 
 @main.route('/adopt-new-yorks-past')
 def new_yorks_past():
@@ -123,7 +48,7 @@ def new_yorks_past():
 
 @main.route('/adopt-new-yorks-past/item/<item_id>')
 def new_yorks_past_view_item(item_id):
-    item = next((item for item in items if str(item['id']) == item_id), None)
+    item = next((item for item in items if item['id'] == item_id), None)
     if item is None:
         return "Item not found", 404
     return render_template('items/view_item.html', item=item, PAYPAL_CLIENT_ID=PAYPAL_CLIENT_ID)
