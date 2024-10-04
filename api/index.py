11e3c6a0414e6ea86010c4ea947e89e661ef533f
nycarchivalsociety <nycarchivalsociety@gmail.com>
2024-10-04 16:18:13 -0700

    Fix Vercel runtime configuration

--- api/index.py
+++ api/index.py
@@ -11,8 +11,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    app.run(debug=True)
-=======
     app.run()
->>>>>>> abde80176b5c71c88018e77bebbba9b0f228425c
