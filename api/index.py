cf69fa802d0dc6259071aa8b0680edd3c6807f93
nycarchivalsociety <142239463+nycarchivalsociety@users.noreply.github.com>
2024-10-03 23:43:30 -0400

    Add files via upload

---
+++ api/index.py
@@ -0,0 +1,14 @@
+# api/index.py
+
+import sys
+import os
+
+# Add the parent directory to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
